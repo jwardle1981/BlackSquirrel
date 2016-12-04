8599145d04c548d2dec45ed5bf77370a69411a36
Jimmy Wardle <wardlejames81@gmail.com>
2016-12-03 23:05:56 -0500

    added a home page, about page, models page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  root'pages#home'
+  	get 'about' => 'pages#about'
+  	get 'contact' => 'pages#contact'
+  	get 'models' => 'pages#models'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
